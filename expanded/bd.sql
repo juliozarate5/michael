e7a4a017f4b7ef863c51e19c1461df19af4ce5ac
jmartinez <jmartinez@cobiscorp.com>
2020-12-10 02:02:43 -0500

    Se construyen pruebas unitarias y diseño de despliegue en lambda AWS

--- expanded/bd.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS clients;
-CREATE TABLE clients(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(250) NOT NULL,
-    last_name VARCHAR(250) NOT NULL,
-    email VARCHAR(250) DEFAULT NULL
-);
--- expanded/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO clients (first_name, last_name, email) VALUES ('Julio', 'Martinez', 'juliomartinez@gmail.com');
-INSERT INTO clients (first_name, last_name, email) VALUES ('Carolina', 'Pulgarin', 'caro@hotmail.com');
-INSERT INTO clients (first_name, last_name, email) VALUES ('Juliana', 'Martinez', 'jmp@gmail.com');
